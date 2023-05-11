e6be6cc23c613c3c14f5078df20a7aaf983276b0
Orlando Saanchez <castaneda.kevin@uabc.edu.mx>
2023-05-10 20:41:14 -0700

    render components in login interface working

--- blank.jsx
+++
@@ -1,6 +0,0 @@
-/* eslint-disable react/react-in-jsx-scope */
-/* eslint-disable indent */
-const Blank = () => {
-    return <div>Blank page</div>
-}
-export default Blank
--- js/menu.jsx
+++ js/menu.jsx
@@ -2,14 +2,14 @@ import * as React from 'react'
 import { Route, Routes, BrowserRouter } from 'react-router-dom'
 import { ChakraProvider } from '@chakra-ui/react'
 import * as ReactDOM from 'react-dom/client'
-import SimpleCard from './src/components/login'
-const rootElement = document.getElementById('app')
+import Example from '../src/components/navbar'
+const rootElement = document.getElementById('menu app')
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <ChakraProvider>
       <BrowserRouter>
         <Routes>
-          <Route exact path='/' element={<SimpleCard/>} />
+          <Route exact path='/routes/menu.html' element={<Example/>} />
         </Routes>
       </BrowserRouter>
     </ChakraProvider>
